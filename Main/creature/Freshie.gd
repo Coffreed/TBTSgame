4f67411f6b45b130fc115fadc215d4c7f05f9cf8
PUUSTED <edward.a.puustinen@gmail.com>
2020-02-13 22:38:06 +0200

    Added box select missing how to display it and 2 seperate teams

--- Main/creature/Freshie.gd
+++ Main/creature/Freshie.gd
@@ -1,9 +1,19 @@
 extends KinematicBody
 
+export var team = 0
+var team_colors = {
+	0: preload("res://Main/creature/Divine.tres"),
+	1: preload("res://Main/creature/Hellish.tres")
+}
+
 var path = []
 var path_ind = 0
 const move_speed = 12
 onready var nav = get_parent()
+
+func _ready():
+	if team in team_colors:
+		$MeshInstance.material_override = team_colors[team]
 
 func move_to(target_pos):
 	path = nav.get_simple_path(global_transform.origin, target_pos)
@@ -16,3 +26,9 @@ func _physics_process(delta):
 			path_ind += 1
 		else:
 			move_and_slide(move_vec.normalized() * move_speed, Vector3(0, 1, 0))
+
+func select():
+	$SelectionRing.show()
+	
+func deselect():
+	$SelectionRing.hide()
---
+++ Main/theBin/binCode.cs
@@ -0,0 +1,22 @@
+/*using Godot;
+using System;
+
+public class binCode : Node
+{
+
+	public override void _Ready()
+	{
+		
+	}
+
+}*/
+
+/*
+ * Godot code
+*/
+/*
+func move_all_units(m_pos):
+	var result = raycast_from_mouse(m_pos, 1)
+	if result:
+		get_tree().call_group("units", "move_to", result.position)
+*/
